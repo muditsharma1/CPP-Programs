55610a2c44f154f0b5377ecf356ee2251c01b00f
Mudit Sharma <muditsharma23702@gmail.com>
2020-05-09 14:20:36 +0530

    Add files via upload

---
+++ InputOutput.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 2, b = 3;
+    float pi = 3.14;
+    char m = 'm';
+    cout << "a= " << a << " \nb= " << b << "\npi= " << pi << "\nm= " << m;
+    return 0;
+}

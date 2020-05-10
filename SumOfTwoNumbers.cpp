c35e646a4e3adc43ed6c354b885efbe4b67428eb
Mudit Sharma <muditsharma23702@gmail.com>
2020-05-10 15:16:14 +0530

    Add files via upload

---
+++ SumOfTwoNumbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    cout << "Enter the Value Of number 1:";
+    cin >> num1;
+    cout << "enter the value of number 2:";
+    cin >> num2;
+    cout << "the sum =" << num1 + num2;
+    return 0;
+}                                                                                           

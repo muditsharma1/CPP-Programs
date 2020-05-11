919c3db82b945b4003ae786e82ffb63006fcfd4c
Mudit Sharma <muditsharma23702@gmail.com>
2020-05-11 16:56:47 +0530

    Add files via upload

---
+++ ArithmaticAndComparisonOperators.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //arithmatic operator
+    int a = 5, b = 4;
+    cout << "a+b=" << a + b << endl;
+    cout << "a-b=" << a - b << endl;
+    cout << "a*b=" << a * b << endl;
+    cout << "a/b=" << a / b << endl;
+    cout << "a%b=" << a % b << endl;
+    cout<<endl;
+    //comparison operator
+    cout << "a==b=" << (a == b) << endl;
+    cout << "a!=b=" << (a != b) << endl;
+    cout << "a>=b=" << (a >= b) << endl;
+    cout << "a<=b=" << (a <= b) << endl;
+    cout << "a<b=" << (a < b) << endl;
+    cout << "a>b=" << (a > b) << endl;
+    return 0;
+}
